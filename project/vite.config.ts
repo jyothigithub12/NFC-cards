33c6b7050b0e646f05111cb579ea325bbc806056
jyothigithub12 <144435403+jyothigithub12@users.noreply.github.com>
2025-07-06 16:11:33 +0530

    Update vite.config.ts

--- project/vite.config.ts
+++ project/vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/diplomat-business-cards/', // Replace with your actual repository name
+  base: '/NFC-cards/', // Replace with your actual repository name
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
